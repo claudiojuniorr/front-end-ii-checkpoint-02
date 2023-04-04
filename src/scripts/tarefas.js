bffa4e2d3f6ff55430aa74fe0977332e3356833f
Claúdio Ribeiro Junior <seuEmail>
2023-04-04 12:25:35 -0300

    feat:updated-script-tarefas

--- src/scripts/tarefas.js
+++ src/scripts/tarefas.js
@@ -4,25 +4,7 @@ const buttonCreateTaskRef = document.querySelector('#createTask')
 const pendingTasksRef = document.querySelector('.tarefas-pendentes')
 const finishedTasksRef = document.querySelector('.tarefas-terminadas')
 const inputNewTaskRef = document.querySelector('#novaTarefa')
-
 const buttonTaskRef = document.querySelectorAll('#buttonTask')
-
-function addEventListenerTask(tarefa){
-    const taskUnfinishRef = document.querySelectorAll('.tarefas-pendentes .tarefa')
-    const taskFinishRef = document.querySelectorAll('.tarefas-terminadas .tarefa')
-
-    const arrayTaskUnfinishRef = Array.from(taskUnfinishRef)
-    arrayTaskUnfinishRef.map((item, index) => {
-        const taskRef = item.children[0]
-        taskRef.addEventListener('click', () => editTask(unfinishedTasks[index].id))
-    })
-    
-    const arrayTaskFinishRef = Array.from(taskFinishRef)
-    arrayTaskFinishRef.map((item, index) => {
-        const taskRef = item.children[0]
-        taskRef.addEventListener('click', () => deleteTask(finishedTasks[index].id))
-    })
-}
 
 var tarefa = {
     description: '',
@@ -70,6 +52,23 @@ function sortArray(array){
         if(a.id > b.id) return 1
         if(a.id < b.id) return -1
         return 0
+    })
+}
+
+function addEventListenerTask(tarefa){
+    const taskUnfinishRef = document.querySelectorAll('.tarefas-pendentes .tarefa')
+    const taskFinishRef = document.querySelectorAll('.tarefas-terminadas .tarefa')
+
+    const arrayTaskUnfinishRef = Array.from(taskUnfinishRef)
+    arrayTaskUnfinishRef.map((item, index) => {
+        const taskRef = item.children[0]
+        taskRef.addEventListener('click', () => editTask(unfinishedTasks[index].id))
+    })
+    
+    const arrayTaskFinishRef = Array.from(taskFinishRef)
+    arrayTaskFinishRef.map((item, index) => {
+        const taskRef = item.children[0]
+        taskRef.addEventListener('click', () => deleteTask(finishedTasks[index].id))
     })
 }
 
